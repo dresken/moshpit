7029838563c9127bdf66844ec2e505690f0e1cb1
Aaron Howell <github-projects@roots.id.au>
2012-12-25 10:02:34 +1100

    Update google anayltics to h5bp template
    Need to optimise further as SSL detection can be done server side

--- site/bones.class.php
+++ site/bones.class.php
@@ -200,7 +200,7 @@ abstract class Bones extends \Moshpit\HttpChat {
         return$this->adminArea;
     }
 
-
+    //TODO: refine to optimise javascript further by leveraging PHP detection of SSL
     protected function addGoogleAnalytics($id, $protectedArea) {
         if (// Not in admin area
                 ! preg_match('|^/'.$protectedArea.'/|',$_SERVER["REQUEST_URI"])
@@ -211,18 +211,12 @@ abstract class Bones extends \Moshpit\HttpChat {
                 // Not from home
                 && gethostbyname('moshpit.doesntexist.org') != $_SERVER["REMOTE_ADDR"]
         ) {
-            $this->addHead('<script type="text/javascript">
-              <!--
-              var _gaq = _gaq || [];
-              _gaq.push(["_setAccount", "'.$id.'"]);
-              _gaq.push(["_trackPageview"]);
-              (function() {
-                var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;
-                ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
-                var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);
-              })();
-              //-->
-        </script>'
+            $this->addHead("<script>
+            var _gaq=[['_setAccount','<?php echo $config->google_analytics; ?>'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>"
             );
         }
     }
