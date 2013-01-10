fd4c1c9ae7e2490711396800257831b61b9a165d
Aaron Howell <github-projects@roots.id.au>
2013-01-10 19:04:55 +1100

    Config files can be read as text and saved back
    Use single variable to store data
    implemented toString and toArray

--- moshpit/config.class.php
+++ moshpit/config.class.php
@@ -1,42 +1,117 @@
 <?php
 namespace Moshpit;
 final class Config {
-    private $_CONFIG;
+    private static $_config;
+
+    public static function Config($filename=NULL) {
+        if(!isset(self::$_config)) {
+            self::$_config = new Config();
+            if ($filename !== NULL) {
+                try {
+                    self::$_config->load($filename);
+                } catch (\Exception $e) {
+                    self::$_config = NULL;
+                    throw $e;
+                }
+            }
+        }
+        
+        return self::$_config;
+    }
+    
+    const CHILD = '->';
+    const EQUALS = '=';
+    
     private $_VALUE;
             
-    public function __construct($value=NULL) {
-        $this->_CONFIG = array();
+    public function __construct($value=array()) {
         $this->_VALUE = $value;
     }
     
     public function __set($name, $value) {
-        $this->_CONFIG[$name] = new Config($value);
+        if (array_key_exists($name, $this->_VALUE))
+            throw new \Exception("Config option already defined: $name");
+        
+        $this->add($name, $value);
+    }
+    
+    public function isValue() {
+        return ! is_array($this->_VALUE);
+    }
+    
+    private function add($key, $value=array()) {
+        $this->_VALUE[$key] = new Config($value);
     }
     
     public function __get($name) {
-        if ($name == '_VALUE') {
+        if ($name == '_VALUE')
             return $this->_VALUE;
-        } elseif (array_key_exists($name, $this->_CONFIG)) {
-            return $this->_CONFIG[$name]->_VALUE;
+        
+        if ($this->isValue())
+            throw new \Exception("No further attributes stored");
+            
+        if (! array_key_exists($name, $this->_VALUE)) {
+            $this->add($name);
         }
-
-        /*$trace = debug_backtrace();
-        trigger_error(
-            'Undefined property via __get(): ' . $name .
-            ' in ' . $trace[0]['file'] .
-            ' on line ' . $trace[0]['line'],
-            E_USER_NOTICE);
-         */
-        return NULL;
+            
+        if ($this->_VALUE[$name]->isValue()) {
+            return $this->_VALUE[$name]->_VALUE;
+        } else {
+            return $this->_VALUE[$name];
+        }
     }
     
-    //TODO: Make config not require a require
     public function load($filename) {
-        $config = $this;
-        require "$filename";
-        return $config;
+        $file = file($filename);
+        if (! $file)
+            throw new \Exception("File does not exists: ".$filename);
+
+        foreach ($file as $line) {
+            $operands = array_map('trim',explode(self::EQUALS, $line,2));
+            $names = explode(self::CHILD,$operands[0]);
+            $last = array_pop($names);
+            $config = $this;
+            foreach ($names as $name) {
+                $config = $config->$name;
+            }
+            $config->$last = $operands[1];
+        }
     }
     
-
+    public function save($filename) {
+        file_put_contents($filename, $this->toString());
+    }
+    
+    public function toArray() {
+        if ($this->isValue()) {
+            return $this->_VALUE;
+        } else {
+            ksort($this->_VALUE);
+            $result = array();
+            foreach ($this->_VALUE as $name => $value) {
+                $result[$name] = $value->toArray();
+            }
+            return $result;
+        }
+    }
+    
+    public function toString($parent=array()) {
+         if ($this->isValue()) {
+            return implode(self::CHILD, $parent).self::EQUALS.$this->_VALUE."\n";
+        } else {
+            ksort($this->_VALUE);
+            $result = '';
+            foreach ($this->_VALUE as $name => $value) {
+                $new_parent = $parent;
+                $new_parent[] = $name;
+                $result .= $value->toString($new_parent);
+            }
+            return $result;
+        }
+    }
+    
+    public function __toString() {
+        return $this->toString();
+    }
 }
 ?>
